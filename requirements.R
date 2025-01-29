a634eabc20b5c43559fe9a6bf3edb524fcb650a1
Mitch Greer <magreer@ualberta.ca>
2025-01-28 17:16:55 -0700

    use pak for ReturnForge

--- requirements.R
+++ requirements.R
@@ -1,5 +1,9 @@
-p <- c("ReturnForge","PerformanceAnalytics","ggplot2","tidyverse") #Return forge probably isn't going to work because it isn't on Cran
+p <- c("PerformanceAnalytics","ggplot2","tidyverse") #Return forge probably isn't going to work because it isn't on Cran
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
   install.packages(new.packages, dependencies = TRUE)
+}
+
+if (!"ReturnForge" %in% installed.packages()[, "Package"]) {
+  pak::pak("UoASchool-of-Finance/ReturnForge")
 }
